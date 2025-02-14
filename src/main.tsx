f530e51d338183ad184059f0d046c7f4d1586069
Jin <jinx.educ.kuma@gmail.com>
2025-02-14 17:54:59 +0900

    初期設定完了

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+  uri: "http://localhost:8080/v1/graphql",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <BrowserRouter basename="/libraflow">
+      <Routes>
+        <Route path="/login" element={<>LoginPage</>} />
+        <Route path="/main" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </ApolloProvider>
+);
