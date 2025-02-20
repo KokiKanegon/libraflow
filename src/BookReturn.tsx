55cbe1a6052518510ae1bd8fc29069127a5d8e24
Koki Imaeda <koki130@icloud.com>
2025-02-20 13:48:30 +0900

    Checkboxコンポーネントを追加し、@radix-ui/react-checkbox依存関係をpackage.jsonに追加

--- src/BookReturn.tsx
+++ src/BookReturn.tsx
@@ -20,19 +20,13 @@ import {
   TableHeader,
   TableRow,
 } from "./components/ui/table";
+import { Checkbox } from "./components/ui/checkbox";
+import { set } from "react-hook-form";
 // import { set } from "react-hook-form";
 // import { graphql } from "./gql/gql";
 
-// 検索用クエリ
-const testqery = gql(`
-  query GetBookByCode {
-  libraflow_t_book(where: {book_code: {_eq: "1122"}}) {
-    id
-  }
-}
-`);
-
-// ユーザー検索クエリ
+// クエリ部分
+// 1. ユーザー検索クエリ
 const GET_USER = gql(`
   query GetUserByCode($user_code: String!) {
   libraflow_m_user(where: {user_code: {_eq: $user_code}}) {
@@ -43,80 +37,91 @@ const GET_USER = gql(`
 }
 `);
 
-//
-const GET_BOOK_BY_USER = gql(`
-query GetBookByUser($user_id: uuid!) {
-  libraflow_t_borrow_record(order_by: {borrow_date: desc}, where: {m_user_id: {_eq: $user_id}, return_date: {_is_null: true}}) {
-    id
-    t_book {
-    book_code
-    title
-    author
-    isbn_code
-    }
-  }
-}
-`);
-
-const INSERT_REGISTERATION = gql(`
-mutation InsertBookRecord(
-    $m_user_id: uuid!
-    $t_book_id: uuid!
-    $borrow_date: date!
-  ) {
-    insert_libraflow_t_borrow_record(
-      objects: [
-        {
-          m_user_id: $m_user_id
-          t_book_id: $t_book_id
-          borrow_date: $borrow_date
-        }
-      ]
-    ) {
-      returning {
-        id
+// 2. 貸出中の本の表示
+const GET_REGISTER_BY_USER = gql(`
+  query GetBookByUser($m_user_id: uuid!) {
+    libraflow_t_borrow_record(order_by: {borrow_date: asc}, where: {m_user_id: {_eq: $m_user_id}, return_date: {_is_null: true}}) {
+      id
+      t_book {
+      book_code
+      title
+      author
+      isbn_code
       }
     }
   }
-`);
+  `);
+
+// 3. 返却機能（Update）
+const RETURN_BOOK = gql(`
+  mutation ReturnBook(
+      $id: [uuid!]
+      $return_date: date!
+    ) {
+      update_libraflow_t_borrow_record(
+        where: {
+          id: {_in: $id}}, 
+          _set: {return_date: $return_date}
+        ) {
+    returning {
+      id
+    }
+  }
+}
+  `);
+
+// function dateFormat -> change date format
+function dateFormat(today: any, format: any) {
+  format = format.replace("YYYY", today.getFullYear());
+  format = format.replace("MM", ("0" + (today.getMonth() + 1)).slice(-2));
+  format = format.replace("DD", ("0" + today.getDate()).slice(-2));
+  return format;
+}
 
 export default function BookReturn() {
-  const [searchCode, set_searchCode] = useState<string>("");
+  // 管理するステート
+  // 1. ユーザーコード
   const [searchUserCode, set_searchUserCode] = useState<string>("");
-  const [book_code_list, set_book_code_list] = useState<string[]>([]); // 書籍コードリスト
 
-  function dateFormat(today: any, format: any) {
-    format = format.replace("YYYY", today.getFullYear());
-    format = format.replace("MM", ("0" + (today.getMonth() + 1)).slice(-2));
-    format = format.replace("DD", ("0" + today.getDate()).slice(-2));
-    return format;
-  }
+  // 2. 貸出中の本のコードリスト
+  // const [borrow_id_list, set_borrow_id_list] = useState<string[]>([]); // 書籍コードリスト
 
+  // 3. 返却予定の本のリスト
+  const [return_id_list, set_return_id_list] = useState<string[]>([]); // 書籍コードリスト
+
+  // 4. ユーザーコードと返却日
   const [formState, setFormState] = useState({
     m_user_id: "",
-    borrow_date: dateFormat(new Date(), "YYYY-MM-DD"),
-  }); // 編集用データ
-
-  useEffect(() => {
-    if (book_code_list) {
-      getquery({ variables: { book_codes: book_code_list ?? [] } });
-    }
-  }, [book_code_list]);
-
-  // 表示用書籍データを取得
-  // const [getquery, { data: tableData }] = useLazyQuery(testqery, {
-  const [getquery, { data: tableData }] = useLazyQuery(GET_BOOK_BY_USER, {
-    onCompleted: (tableData) => {
-      console.log(tableData);
-    },
+    return_date: dateFormat(new Date(), "YYYY-MM-DD"),
   });
 
-  // 表示用書籍データを取得
-  const [
-    getuserquery,
-    // { data: userData, loading: userLoading, error: userError },
-    { data: userData },
-  ] = useLazyQuery(GET_USER, {
+  useEffect(() => {
+    if (formState.m_user_id) {
+      getquery({ variables: { m_user_id: formState.m_user_id ?? [] } });
+    }
+  }, [formState.m_user_id]);
+
+  useEffect(() => {
+    if (return_id_list) {
+      console.log(return_id_list);
+    }
+  }, [return_id_list]);
+
+  // function部分
+  // 1. ユーザー検索
+  const clickUserSearch = () => {
+    if (searchUserCode) {
+      getuserquery({ variables: { user_code: searchUserCode } });
+      if (userData) {
+        setFormState((prev) => ({
+          ...prev,
+          m_user_id: userData.libraflow_m_user[0].id,
+        }));
+      }
+    }
+  };
+
+  const [getuserquery, { data: userData }] = useLazyQuery(GET_USER, {
     onCompleted: (userData) => {
       if (userData.libraflow_m_user.length === 0) {
         alert("No user found.");
@@ -131,31 +136,24 @@ export default function BookReturn() {
     },
   });
 
-  const clickBookSearch = () => {
-    if (searchCode) {
-      checkquery({ variables: { a: searchCode } });
-    }
-  };
+  // 2. 貸出中の本の表示
+  // const [getquery, { data: tableData }] = useLazyQuery(testqery, {
+  const [getquery, { data: tableData }] = useLazyQuery(GET_REGISTER_BY_USER, {
+    onCompleted: (tableData) => {
+      console.log(tableData);
+    },
+    onError: (error) => {
+      console.error("Error fetching data:", error);
+    },
+  });
 
-  const clickUserSearch = () => {
-    if (searchUserCode) {
-      getuserquery({ variables: { user_code: searchUserCode } });
-    }
-  };
-
-  const clickDeleteBook: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    const value = e.currentTarget.value;
-    if (book_code_list.includes(value)) {
-      set_book_code_list((prev) => prev.filter((code) => code !== value));
-    }
-  };
-
-  const [insertRegistration] = useMutation(INSERT_REGISTERATION);
+  // 3. 返却機能（Update）
+  const [returnRegistration] = useMutation(RETURN_BOOK);
 
   const clickOnSubmit: React.MouseEventHandler<HTMLButtonElement> = async (
     e
   ) => {
-    if (tableData?.libraflow_t_book.length === 0) {
+    if (return_id_list.length === 0) {
       alert("No book selected.");
       return;
     }
@@ -163,74 +161,49 @@ export default function BookReturn() {
       alert("No user selected.");
       return;
     }
-    if (!formState.borrow_date) {
-      alert("No borrow date selected.");
+    if (!formState.return_date) {
+      alert("No return date selected.");
       return;
     }
-    e.preventDefault();
 
-    //貸出中チェック
-    for (let i = 0; i < tableData.libraflow_t_book.length; i++) {
-      if (tableData.libraflow_t_book[i].t_borrow_records.length > 0) {
-        alert(
-          `Book ${tableData.libraflow_t_book[i].book_code} is already borrowed.`
-        );
-        set_book_code_list((prev) =>
-          prev.filter(
-            (code) => code !== tableData.libraflow_t_book[i].book_code
-          )
-        );
-        return;
-      }
-    }
-
-    const { m_user_id, borrow_date } = formState;
+    const { m_user_id, return_date } = formState;
     let message = "";
 
     try {
-      // Promise.all を使い、すべての非同期処理を並列実行
-      const insertions = tableData.libraflow_t_book.map((book: any) =>
-        insertRegistration({
+      return_id_list.map((return_id_record) => {
+        returnRegistration({
           variables: {
-            m_user_id: m_user_id,
-            borrow_date: borrow_date,
-            t_book_id: book.id,
+            id: return_id_record,
+            return_date: return_date,
           },
         }).then(() => {
-          console.log("Book updated successfully!", book.book_code);
-          message = book.book_code; // 最後に登録した book_code をセット
-        })
-      );
-
+          console.log("Book updated successfully!", return_id_record);
+          message = return_id_record; // 最後に登録した book_code をセット
+        });
+      });
       // すべての insert が完了するまで待機
-      await Promise.all(insertions);
+      await Promise.all(return_id_list);
 
-      alert(`Book(s) updated successfully! Last updated: ${message}`);
+      alert(`Book(s) updated successfully! Last updated:`);
+      setFormState((prev) => ({
+        ...prev,
+        m_user_id: userData.libraflow_m_user[0].id,
+      }));
     } catch (err) {
       console.error("Error updating book:", err);
       alert("An error occurred while updating the book.");
     }
   };
 
-  const clickBorrowedList = () => {
-    if (!formState.m_user_id) {
-      alert("No user selected.");
-      return;
-    }
-    alert(formState.m_user_id);
-
-    getquery({ variables: { m_user_id: formState.m_user_id ?? [] } });
-  };
-
   return (
+    // 画面周り
     <>
       {/* // タイトル表示 */}
       <div>
         <h1> Book Return Page</h1>
-        <p> 本を読み取ってください。</p>
+        <p> ユーザー情報を入力してください</p>
       </div>
-      {/* // 検索画面（Inputbox and SearchButton） */}
-
+      {/* // 1. ユーザー検索 */}
       <Card className="user-card">
         <CardHeader>
           <Label>貸出情報</Label>
@@ -259,36 +232,44 @@ export default function BookReturn() {
         </CardContent>
       </Card>
 
-      <Button onClick={clickBorrowedList}>貸出中のリストを表示</Button>
-
+      {/* ; // 2. 貸出中の本の表示（返却するものにチェックボックスをつけてそれで返却を制御する） */}
       <Card>
         <CardContent>
           <Label>予約候補</Label>
           <Table>
             <TableHeader>
               <TableRow>
+                <TableHead>選択</TableHead>
                 <TableHead>コード</TableHead>
                 <TableHead>タイトル</TableHead>
                 <TableHead>著者</TableHead>
                 <TableHead>ISBNコード</TableHead>
                 <TableHead>貸出中</TableHead>
-                <TableHead>解除</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {tableData?.libraflow_t_book?.map((book: any) => (
+              {tableData?.libraflow_t_borrow_record?.map((book: any) => (
                 <TableRow key={book.id}>
-                  <TableCell>{book.book_code}</TableCell>
-                  <TableCell>{book.title}</TableCell>
-                  <TableCell>{book.author}</TableCell>
-                  <TableCell>{book.isbn_code}</TableCell>
+                  <TableCell key={book.id}>
+                    <Checkbox
+                      id={book.id}
+                      onCheckedChange={(e) => {
+                        if (e) {
+                          set_return_id_list((prev) => [...prev, book.id]);
+                        } else {
+                          set_return_id_list((prev) =>
+                            prev.filter((id) => id !== book.id)
+                          );
+                        }
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>{book.t_book.book_code}</TableCell>
+                  <TableCell>{book.t_book.title}</TableCell>
+                  <TableCell>{book.t_book.author}</TableCell>
+                  <TableCell>{book.t_book.isbn_code}</TableCell>
                   <TableCell>
                     {/* {book.t_borrow_records[0].m_user.user_name} */}
-                  </TableCell>
-                  <TableCell>
-                    <Button value={book.book_code} onClick={clickDeleteBook}>
-                      外す
-                    </Button>
                   </TableCell>
                 </TableRow>
               ))}
@@ -297,10 +278,26 @@ export default function BookReturn() {
         </CardContent>
       </Card>
 
-      {/* // 貸出冊数の表示 */}
-      <Label>現在の冊数：{book_code_list.length ?? 0}冊</Label>
-      {/* // 貸出ボタン */}
-      <Button onClick={clickOnSubmit}>返却</Button>
+      {/* // 3. 返却ボタン（返却処理を実行　返却日は指定） */}
+      <Card>
+        <CardContent>
+          <Label>返却日</Label>
+          <Input
+            type="date"
+            value={formState.return_date}
+            onChange={(e) => {
+              setFormState((prev) => ({
+                ...prev,
+                return_date: e.target.value,
+              }));
+            }}
+          />
+          {/* // 貸出冊数の表示 */}
+          <Label>現在の冊数：{return_id_list.length ?? 0}冊</Label>
+          {/* // 貸出ボタン */}
+          <Button onClick={clickOnSubmit}>返却</Button>
+        </CardContent>
+      </Card>
     </>
   );
 }
